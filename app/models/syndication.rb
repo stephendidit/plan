1608b813a90cc6ffa9fb5b87d31508a9e3d0da6d
stephendidit <stephendidit@gmail.com>
2016-06-23 13:52:36 +0000

    Move filtering from txt.erb files to syndication model

--- app/models/syndication.rb
+++ app/models/syndication.rb
@@ -1,3 +1,7 @@
 class Syndication < ActiveRecord::Base
 
+    scope :most_recent, -> (timestamp){ where(created_at: timestamp) }
+    scope :slides, -> { where(name: 'tour_slides_descriptions.txt').where.not(body: 'Old Feed') }
+    scope :urls, -> { where(name: 'tour_urls.txt').where.not(body: 'Old Feed') }
+
 end
