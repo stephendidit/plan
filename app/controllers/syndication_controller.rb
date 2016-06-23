2027ed89a9933a0a1800d93b9a1c5675cb300a86
stephendidit <stephendidit@gmail.com>
2016-06-23 02:57:08 +0000

    Set up text feeds

--- app/controllers/syndication_controller.rb
+++ app/controllers/syndication_controller.rb
@@ -1,7 +1,13 @@
 class SyndicationController < ApplicationController
+  def initialize
+    @syndications = Syndication.all
+  end
+  
   def tour_urls
+    render './syndication/tour_urls.txt.erb', layout: false, content_type: 'text/plain'
   end
 
   def tour_slides_descriptions
+    render './syndication/tour_slides_descriptions.txt.erb', layout: false, content_type: 'text/plain'
   end
 end
